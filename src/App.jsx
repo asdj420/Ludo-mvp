a5377c9492ee216cc0c66ce54b5d320eb8d4b7e9
asdj420 <houseofrajatva@gmail.com>
2025-06-26 03:20:49 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LudoBoard from "./components/LudoBoard";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-green-100">
+      <LudoBoard />
+    </div>
+  );
+}
